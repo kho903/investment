e87635c39218a057ca74bad144745f5659aace0f
kho903 <gmldnr2222@naver.com>
2022-01-09 19:33:06 +0900

    feat: add Investment / Product repository

--- src/main/java/com/fastcampus/investment/repository/ProductRepository.java
+++ src/main/java/com/fastcampus/investment/repository/ProductRepository.java
@@ -3,6 +3,10 @@ package com.fastcampus.investment.repository;
 import com.fastcampus.investment.entity.Products;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Products, Long> {
 
+    List<Products> findByStartedAtBeforeAndFinishedAtAfter(LocalDateTime startedAt, LocalDateTime finishedAt);
 }
