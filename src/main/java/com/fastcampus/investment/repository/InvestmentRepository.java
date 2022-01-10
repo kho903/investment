fda241940b0e314a45156c45654aa1ec98e5a89a
kho903 <gmldnr2222@naver.com>
2022-01-10 13:17:14 +0900

    feat: add sumOfInvestedAmount in InvestmentRepository and getValidProducts() in ProductService

--- src/main/java/com/fastcampus/investment/repository/InvestmentRepository.java
+++ src/main/java/com/fastcampus/investment/repository/InvestmentRepository.java
@@ -1,7 +1,15 @@
 package com.fastcampus.investment.repository;
 
 import com.fastcampus.investment.entity.Investment;
+import com.fastcampus.investment.entity.InvestmentStatus;
+import com.fastcampus.investment.entity.Products;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface InvestmentRepository extends JpaRepository<Investment, Long> {
+
+    Long countByProductAndInvestmentStatus(Products products, InvestmentStatus investmentStatus);
+
+    @Query("select COALESCE(sum(i.investedAmount), 0) from Investment i where i.product.id = :#{#product.id}")
+    Long sumOfInvestedAmount(Products product);
 }
---
+++ src/main/java/com/fastcampus/investment/service/ProductService.java
@@ -0,0 +1,36 @@
+package com.fastcampus.investment.service;
+
+import com.fastcampus.investment.dto.ProductDto;
+import com.fastcampus.investment.entity.InvestmentStatus;
+import com.fastcampus.investment.entity.Products;
+import com.fastcampus.investment.repository.InvestmentRepository;
+import com.fastcampus.investment.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductRepository productRepository;
+    private final InvestmentRepository investmentRepository;
+
+    public List<ProductDto> getValidProducts() {
+        LocalDateTime now = LocalDateTime.now();
+        List<Products> productsList = productRepository.findByStartedAtBeforeAndFinishedAtAfter(now, now);
+        List<ProductDto> result = new ArrayList<>();
+
+        for (Products products : productsList) {
+            ProductDto productDto = products.toDto();
+            productDto.setInvestedAmount(investmentRepository.sumOfInvestedAmount(products));
+            productDto.setInvestedCount(investmentRepository.countByProductAndInvestmentStatus(products, InvestmentStatus.INVESTED));
+            result.add(productDto);
+        }
+
+        return result;
+    }
+}
