d0fedaf8df18688fc9f7b2da7c37ab6711d3ec6c
kho903 <gmldnr2222@naver.com>
2022-01-10 14:48:03 +0900

    test: makeProductTest and findByStartedAtBeforeAndFinishedAtAfterTest

--- src/test/java/com/fastcampus/investment/repository/ProductRepositoryTest.java
+++ src/test/java/com/fastcampus/investment/repository/ProductRepositoryTest.java
@@ -4,6 +4,10 @@ import com.fastcampus.investment.entity.Products;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,14 +18,49 @@ class ProductRepositoryTest {
     ProductRepository productRepository;
 
     @Test
-    void repositoryTest() {
-        Products product = new Products();
-        product.setTitle("첫번째 제품");
-        product.setId(1L);
-
+    void makeProductTest() {
+        Products product = makeProduct();
         productRepository.save(product);
 
-        Products findProduct = productRepository.findById(1L).get();
+        Products findProduct = productRepository.findById(5L).get();
         assertThat(findProduct.getTitle()).isEqualTo("첫번째 제품");
+    }
+
+    private Products makeProduct() {
+        return Products.builder()
+                .id(5L)
+                .title("첫번째 제품")
+                .totalInvestAmount(0L)
+                .investedCount(0L)
+                .investedAmount(0L)
+                .startedAt(LocalDateTime.now().minusDays(1))
+                .finishedAt(LocalDateTime.now().plusDays(1))
+                .build();
+    }
+
+    @Test
+    @Transactional
+    void findByStartedAtBeforeAndFinishedAtAfterTest() {
+        Products product = makeProduct();
+        productRepository.save(product);
+        List<Products> result = productRepository.findByStartedAtBeforeAndFinishedAtAfter(LocalDateTime.now(), LocalDateTime.now());
+
+        for (Products products : result) {
+            System.out.println("products.getId() = " + products.getId());
+            System.out.println("products.getTitle() = " + products.getTitle());
+        }
+
+        for (Products products : result) {
+            Products getByRepository = productRepository.getById(products.getId());
+            assertAll(
+                    () -> assertEquals(products.getId(), getByRepository.getId()),
+                    () -> assertEquals(products.getTitle(), getByRepository.getTitle()),
+                    () -> assertEquals(products.getId(), getByRepository.getId()),
+                    () -> assertEquals(products.getInvestedAmount(), getByRepository.getInvestedAmount()),
+                    () -> assertEquals(products.getInvestedCount(), getByRepository.getInvestedCount()),
+                    () -> assertEquals(products.getStartedAt(), getByRepository.getStartedAt()),
+                    () -> assertEquals(products.getFinishedAt(), getByRepository.getFinishedAt())
+            );
+        }
     }
 }
